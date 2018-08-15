a2f23603310035a5adb27d43b91febc69c9162cf
Danny McCardle <danny.mccardle@populusgs.com>
2018-08-15 16:27:09 -0300

    Mobile dropdown menu can now be closed by click away from it. Removed unused js import.

--- script/menu.js
+++ script/menu.js
@@ -6,6 +6,22 @@ $(document).ready(function() {
         $('#menu-bar ul').slideToggle(200);
     });
     
+    /* Allows for the menu dropdown to hide by clicking outside of the menu */
+    $('main').on('click', function() {
+    	if($( "#toggle" ).is( ":visible" )) {
+    		$('#menu-bar ul').slideUp(200);
+    	}
+    });
+    
+    /* Fixes issues when resizing screen */
+    $(window).resize(function() {
+    	if($( "#toggle" ).is( ":hidden" )) {
+    		$('#menu-bar ul').show();
+    	} else {
+    		$('#menu-bar ul').hide();
+    	}
+    });
+    
     $('#menu-bar ul a li').on('click', function(event) {
         $('#menu-bar ul a li').removeClass('active');
         $(event.currentTarget).addClass('active');
