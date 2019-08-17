0f48d58be15997a2aefe6f691750d8579bcdcf04
dmccardle <danny.mccardle@unb.ca>
2019-08-17 20:19:09 -0300

    Formatting consistencies

--- script/pageLoad.js
+++ script/pageLoad.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var $children = $("body").children();
 
     var time = 200;
     $("body").show();
-    $children.each(function() {
+    $children.each(function () {
         var current = $(this);
-        setTimeout(function() {
+        setTimeout(function () {
             current.fadeIn(400);
         }, time);
         time += 800;
--- script/smoothScroll.js
+++ script/smoothScroll.js
@@ -1,53 +1,53 @@
-$(document).ready(function() {
+$(document).ready(function () {
     /* Menu links */
     var navigationLinks = $('.scroll');
-    
-    navigationLinks.on('click', function(event) {
+
+    navigationLinks.on('click', function (event) {
         event.preventDefault();
-        
-        var distance = Math.abs($(this).offset().top -  $(this.hash).offset().top);
-        
-        var timeToScroll = getScrollTime(distance); 
-        
+
+        var distance = Math.abs($(this).offset().top - $(this.hash).offset().top);
+
+        var timeToScroll = getScrollTime(distance);
+
         $('body, html').animate({
             scrollTop: $(this.hash).offset().top - getScrollOffset()
-        }, timeToScroll);        
+        }, timeToScroll);
     });
-    
+
     /* Back to Top */
-    
+
     var backToTop = $('#back-to-top');
-    $(window).scroll(function() {
-        if( $(this).scrollTop() > 300) {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
             backToTop.fadeIn(300);
         } else {
             backToTop.fadeOut(300);
         }
     });
-    
-    backToTop.on('click', function(event){
+
+    backToTop.on('click', function (event) {
         event.preventDefault();
         var scrollDistance = Math.abs($(window).scrollTop());
-        
+
         var topScrollTime = getScrollTime(scrollDistance);
-        
+
         $('body, html').animate({
             scrollTop: 0
-        }, topScrollTime); 
+        }, topScrollTime);
     });
 });
 
-function getScrollOffset() {		
-    return 50;	
+function getScrollOffset() {
+    return 50;
 };
 
 function getScrollTime(dist) {
     var minTime = 300;
     var maxTime = 1000;
     var time = 23 * Math.sqrt(dist);
-    
+
     time = time < minTime ? minTime : time;
     time = time > maxTime ? maxTime : time;
-    
+
     return time;
 }
