7bf2ba46a43dfb3d9bd9dcc8202b69efe91b1a2d
dmccardle-mb <danny@mccardlebros.ca>
2025-05-18 10:55:19 -0300

    deleting old code

--- script/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-
-    $('#involvements .content-box ul li').on('click', function () {
-        $(this).toggleClass('opened');
-        $(this).next('div').toggleClass('hidden');
-    });
-});
